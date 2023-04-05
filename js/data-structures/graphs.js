5b9bf73748b5ffee094e224ef537199098bc1132
Jorge Barron <jorgeb8168711@gmail.com>
2023-04-05 03:03:52 -0600

    Other exercises

---
+++ js/data-structures/graphs.js
@@ -0,0 +1,2 @@
+// graphs
+// https://static.platzi.com/media/user_upload/Post%20Instagram%20%2819%29-6eea4029-c0b4-4a34-8165-c72ae59b1738.jpg
---
+++ js/run-js/deep-freeze.js
@@ -0,0 +1,13 @@
+function deepFreeze (object) {
+  const propNames = Object.getOwnPropertyNames(object)
+
+  for (const name of propNames) {
+    const value = object[name]
+
+    object[name] = value && typeof value === 'object'
+      ? deepFreeze(value)
+      : value
+  }
+
+  return Object.freeze(object)
+}
