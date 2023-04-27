11c43c4b32d0c0d15dbc1f3333396aa5171dbf00
Jorge Barron <jorgeb8168711@gmail.com>
2023-04-26 21:11:58 -0600

    Double linked list examples

--- js/data-structures/doubly-linked-list.js
+++ js/data-structures/doubly-linked-list.js
@@ -1,27 +1,32 @@
 'use strict'
 
 class Node {
-  constructor (value) {
+  constructor (value, prev = null) {
     this.value = value
     this.next = null
+    this.prev = prev
   }
 }
 
-class SinglyLinkedList {
+export default class DoublyLinkedList {
   constructor (value) {
     this.head = new Node(value)
     this.tail = this.head
     this.length = 1
   }
 
+  // add an item at the end of the list
   append (value) {
     const newNode = new Node(value)
+    newNode.prev = this.tail
     this.tail.next = newNode
     this.tail = newNode
+
     this.length += 1
     return this
   }
 
+  // add an item at the start of the list
   prepend (value) {
     const newNode = new Node(value)
     newNode.next = this.head
@@ -37,16 +42,24 @@ class SinglyLinkedList {
       return this.append(value)
     }
 
-    // the newNode is created
+    // the element in which I want to insert the data
     const firstPointer = this.lookup(index - 1)
     if (firstPointer) {
+    // the newNode is created
       const newNode = new Node(value)
-      const holdingPointer = firstPointer.next
+
+      // we backup the next and prev values from the first node
+      const prevHoldingPointer = firstPointer
+      const nextHoldingPointer = firstPointer.next
+
       // then we set the newNode on the firstPointer.next
       firstPointer.next = newNode
-      // we backup the next value from the found node
-      // on the newNode
-      newNode.next = holdingPointer
+
+      // on the newNode we set the next and prev values
+      newNode.prev = prevHoldingPointer
+      newNode.next = nextHoldingPointer
+
+      nextHoldingPointer.prev = newNode
 
       // this means that the new node was inserted at the tail
       // then we need to set the tail with the new node
@@ -75,21 +88,23 @@ class SinglyLinkedList {
   delete (index) {
     if (index === 0) {
       this.head = this.head.next
+      this.head.prev = null
       this.length -= 1
       return this
     }
 
     const firstNode = this.lookup(index - 1)
     const secondNode = this.lookup(index)
+    const secondNodePrev = secondNode.prev
     firstNode.next = secondNode.next
+    firstNode.next.prev = secondNodePrev
+
     this.length -= 1
     return this
   }
 }
 
-const slList = new SinglyLinkedList(1)
-slList.append(2)
-slList.append(3)
-slList.append(8)
-
-slList.delete(3)
+// const dslList = new DoublyLinkedList(1)
+// dslList.append(2)
+// dslList.append(3)
+// dslList.delete(1)
